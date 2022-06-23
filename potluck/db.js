d66bb6e9d2968e922db8793219e021cbed1c27f6
Jordan Wall <jordanthomaswall@gmail.com>
2022-06-23 12:56:29 -0400

    initial commit

---
+++ potluck/db.js
@@ -0,0 +1,110 @@
+const chefs = {
+  gordonRamsay: {
+    firstName: "Kaitlyn",
+    lastName: "Wall",
+    blurb:
+      "Amazing wife and excellent home chef. Favorite recipes include chicken pot pie and Jordan's Homemade Marguaritas",
+    avatar: "",
+    dishes: {
+      dish1: {
+        dishName: "German Potato Salad",
+        dishChefNotes:
+          "A twist on the beloved British favorite, delightfully simple and absolutely delicious for breakfast, brunch, lunch, or even dinner.",
+        dishIngredients: [
+          `1 1/2 pounds (about 8) small red potatoes`,
+          `3 slices (about 4 ounces) thick cut bacon, diced`,
+          `2 medium (3/4 pound) red onions, diced`,
+          `2 tablespoons white wine vinegar`,
+          `1 cup chicken broth (store-bought or homemade)`,
+          `1 tablespoon prepared yellow mustard`,
+          `1/4 teaspoon salt`,
+          `1/4 teaspoon ground black pepper`,
+          `13 cup chopped fresh herbs (I like chives & parsley)`,
+        ],
+        dishRecipe: [
+          `Peel the potatoes. Fill a pot with an inch or two of water and set a steamer basket inside. Bring to a simmer over medium-high heat. Rinse the potatoes and place them in the steamer basket. Steam the potatoes, covered, for 20 minutes. To test for doneness, use a paring knife to pierce one of the potatoes; steam for a few more minutes if not quite done. Remove the steamer basket from the pot and let the potatoes cool for 20 minutes.`,
+          `While the potatoes are cooling, make the bacon dressing: In a large skillet over medium heat, sauté the bacon until it begins to render some of its fat, ~5 min. (For crispier bacon, cook ~8 min.) Add the onions and sauté for 5 more mins, until the onions are softened and translucent, and the bacon is cooked through. Add the vinegar to the skillet. Stir it in with the onions and bacon and let it reduce, about 2 min. Stir in the chicken broth, mustard, salt, and pepper, and turn off the heat. Slice the cooled potatoes about 14 inch thick.`,
+          `Stir the potatoes and herbs into the warm dressing. Stir until the potatoes are coated evenly. Enjoy!`,
+        ],
+        dishImg:
+          "https://www.gordonramsay.com/assets/Uploads/_resampled/CroppedFocusedImage90045050-50-Mushroomtoast.jpg",
+      },
+    },
+  },
+  jordanWall: {
+    firstName: "Jordan",
+    lastName: "Wall",
+    blurb:
+      "Amateur chef and front end developer. Jordan will be the greatest software engineer since mark zuckerberg. He will develop the whole web all by himself.",
+    avatar: "",
+    dishes: {
+      dish1: {
+        dishName: `Jord’s Margs`,
+        dishChefNotes:
+          "Because you’re using fresh fruit, you can get away with pretty darn cheap tequila. I recommend Jimmy Buffet’s margaritaville ($25 for 1.75L). ",
+        dishIngredients: [
+          "750 ml tequila",
+          "500 ml triple sec",
+          "7 limes, halved",
+          "5 lemons, halved",
+          "1 cup water",
+          "1 cup sugar",
+          "Coarse seasalt for serving",
+        ],
+        dishRecipe: [
+          "Make simple syrup. Boil 1 cup water in a small saucepan. Turn off the heat. Add 1 cup sugar and stir until dissolved. Set aside.",
+          "Squeeze lemons and limes into a picture. Add the rinds into the pitcher as well.",
+          "Add triple sec, tequila, and 1 CUP of your simple syrup (save the rest for later).",
+          "Stir vigorously with a wooden spoon for about a minute. This gets all the oils out of the lemon and lime rinds.",
+          "Pour through a strainer to separate the pulp and rinds",
+          "Adjust for taste, and serve over ice in a tall salt-rimmed glass with a lime wege",
+        ],
+        dishImg:
+          "https://www.gordonramsay.com/assets/Uploads/_resampled/CroppedFocusedImage90045050-50-Mushroomtoast.jpg",
+      },
+      dish2: {
+        dishName: `Ching Chong Bing Bong Noodle`,
+        dishChefNotes:
+          "I don't think this dish belongs to any asian cuisine. I just made it using everything I have in my fridge",
+        dishIngredients: [
+          "2 onions sliced",
+          "6 inch thing of lemongrass minced",
+          "6 cloves garlic minced",
+          "3 sq inches fresh ginger minced",
+          "2 limes juiced & zested",
+          "6 boneless chicken thighs (grilled then sliced or whatever. I used leftovers)",
+          "1 package fresh Chinese noodles",
+          "1 bunch of cilantro chopped (separate the stems and the leaves) ",
+          "1 bunch of basil leaves sliced",
+          "1 bell pepper thinly sliced",
+          "Three green onions (slice the green parts into 2 inch slices, the white parts thinly sliced (like 1/8 inches)",
+          "8 birds eye chilis deseeded  (they look just like this emoji. You can sub other hot peppers)",
+          "3 tbsp neutral cooking oil (like canola)",
+          "1 cup cherry tomatoes, halved",
+          "1/4 cup brown sugar",
+          "1/3 cup soy sauce (do not use the low sodium)",
+          "1 tbsp cornstarch",
+          "1 tbsp gochujang",
+          "1 tbsp fish sauce",
+          "1 tbsp sweet soy sauce",
+          "1 tbsp oyster sauce",
+          "1 tbsp black soy sauce",
+          "1 tbsp rice wine vinegar",
+          "A pinch of white pepper",
+        ],
+        dishRecipe: [
+          "Boil water. Toss in noodles. If they’re still frozen, stir and gently pull it apart as it thaws. Should be just past Al Dente. Remove from water in a colander. Rinse with cold water to remove and excess starch and stop the cooking process. Set aside for later.",
+          "In a bowl combine the brown sugar, soy sauce, cornstarch, gochujang, fish sauce, sweet soy sauce, oyster sauce, black soy sauce, rice wine vinegar, and white pepper. Whisk vigorously until you get rid of all the clumps of cornstarch and gochujang.",
+          "Use a food processor, food process the lemongrass, ginger, and garlic until it turns into a paste.",
+          "Get a wok (or a high walled pan ) ripping hot. Add neutral oil. Once you see wisps of little white smoke add ginger, garlic, and lemongrass.  Stir often for about a minute. Add chilis. Open a window or you’ll get emotional. 1-2 minutes.",
+          "Add onions, cilantro stems, cherry tomatoes, and the white part of the green onion. Continue stir frying until onions soft and translucent.",
+          "Add in noodles, sauce, and chicken. Stir frequently, coating everything in the sauce. 2 minutes. Turn off the heat. Add in lime juice, lime zest, remaining cilantro, basil, and remaining green onion. Stir to combine. Taste for seasoning. Adjust accordingly.",
+          "If it needs more salt, add more soy sauce. If it needs more heat, add more sriracha. If it needs more acidity, add more lime juice. If it needs more umami, add more fish sauce. If it needs more sweetness, add more brown sugar.",
+          "Enjoy!",
+        ],
+        dishImg:
+          "https://www.gordonramsay.com/assets/Uploads/_resampled/CroppedFocusedImage90045050-50-Mushroomtoast.jpg",
+      },
+    },
+  },
+};
---
+++ potluck/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I have a lovely bunch of coconuts. There they are a-standing in a row. </h1>
+    </div>
+  );
+}
+
+export default App;
